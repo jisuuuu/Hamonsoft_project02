0af39e0e35f114bb1e84e4049221a1ee340425cf
jisuuuu <gjkl55@naver.com>
2020-03-27 23:25:02 +0900

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\최지수\PycharmProjects\Hamonsoft_project02\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
